0799063483f629d8fea4121a2f4941d19c48fac1
Facundo <facundo.castro.30@hotmail.es>
2023-01-27 16:23:52 -0300

    correcion para usar bootstrap

--- cv.js
+++ cv.js
@@ -1,4 +1,4 @@
 document.getElementById('boton').onclick = function (){
-    console.log("el js funciona")
+    /*console.log("el js funciona") esta linea fue a modo de prueba, al momento de linkear el scrit*/
     document.getElementById('contacto').removeAttribute('hidden')
 }
