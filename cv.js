76d463552a27ee2d3ef3137ea73552a459c5f621
Facundo <facundo.castro.30@hotmail.es>
2023-02-23 09:05:16 -0300

    vuelta de las vacaciones

--- cv.js
+++ cv.js
@@ -1,4 +1,4 @@
 document.getElementById('boton').onclick = function (){
-    /*console.log("el js funciona") esta linea fue a modo de prueba, al momento de linkear el scrit*/
+    /*console.log("el js funciona") esta linea fue a modo de prueba, al momento de linkear el script*/
     document.getElementById('contacto').removeAttribute('hidden')
 }
